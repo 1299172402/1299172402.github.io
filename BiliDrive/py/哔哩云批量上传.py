9cb4f84e020366c14b5e8bb82b6f2a54348ee341
1299172402 <1299172402@qq.com>
2020-02-23 00:46:05 +0800

    物理第二周直播课更新

--- BiliDrive/py/哔哩云批量上传.py
+++ BiliDrive/py/哔哩云批量上传.py
@@ -1,9 +1,11 @@
 import os
 
-path=r'D:\music\inradio'
+path=r'C:\Users\Administrator\AppData\Local\kingsoft\WPS Cloud Files\userdata\qing\filecache\徵羽商的云文档\04 物理\直播课\第二周'
 for i in os.listdir(path):
     print(i)
     a='bdex upload "'+path+'\\' + i +'"'
     #print(a)
     os.system(a)
-    
+
+
+
